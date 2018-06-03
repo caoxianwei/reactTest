c1d51dbefd7b378c037e03503ce2f45167e129dc
sunday <1756862146@qq.com>
2018-06-03 14:58:32 +0800

    add redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import React from 'react';
 // import './App.css';
 // import './es6'
 import {Button} from 'antd-mobile'
+import {createStore} from 'redux'
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case '加机关枪':
+      return state +1
+    case '减机关枪':
+      return state - 1
+    default:
+      return 10
+  }
+}
+const store = createStore(counter)
+const init = store.getState()
+console.log(init)
+function listener(){
+  const current = store.getState()
+  console.log(`现在有机关枪${current}把`)
+}
+
+store.subscribe(listener)
+
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+console.log(store.getState())
 
 class App extends React.Component {
   constructor(props) {
